1fb4430a729075765c56cacbd91ad889637dd3d7
booyaa NA <booyaabooyaabooyaa@gmail.com>
2018-01-11 08:21:52 +0000

    Fix: add code

---
+++ linux/flock/not-safe.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#exit on errors
+set -e
+
+readonly PROGNAME=$(basename "$0" .sh)
+readonly LOCKFILE_DIR=/tmp
+readonly INSTANCE="$1"
+readonly LOCKFILE="${LOCKFILE_DIR}/${PROGNAME}-${INSTANCE}"
+readonly PID="$$"
+
+if [ -z "${INSTANCE}" ]; then
+        echo "Must provide an instance!"
+        exit 1
+fi
+
+# to avoid command block, link file descriptor 200 to our lock file
+exec {lock_fd}>"$LOCKFILE"
+
+# write the pid to the file descriptor
+echo "$$" 1>&${lock_fd}
+
+###################################################################
+## code begins here..
+
+echo "sleeping on $INSTANCE"
+sleep 5
+echo "finished on $INSTANCE"
+exit 0
